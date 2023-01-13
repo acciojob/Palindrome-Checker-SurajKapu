addcb1e30df30bfc123ebe5db461b9cf30dacbfa
SurajKapu <98162452+SurajKapu@users.noreply.github.com>
2023-01-13 18:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+	let left = 0;
+	let right = str.length();
+	while (left < right) {
+		let left_char = str.charAt(left)
+		let right_char = str.charAt(right)
+		if(left_char != right_char)
+			return false
+		left++
+		right--
+	}
+	return true
 }
 module.exports = palindrome
